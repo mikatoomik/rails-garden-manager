1b9bed3fe64c53b4d460b8ecc222c3eff70b7bcf
mikael autheman <mika@atoomik.fr>
2019-12-16 13:01:35 +0100

    add validates on garden and plant model

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,5 +2,5 @@ class Garden < ApplicationRecord
   has_many :plants, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
-  validates :banner_url, presence: true
+  validates :banner_url, presence: true, format: { with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.(sorry for unsplash)'}
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,5 +2,5 @@ class Plant < ApplicationRecord
   belongs_to :garden
 
   validates :name, presence: true, uniqueness: { scope: :garden_id }
-  validates :image_url, presence: true
+  validates :image_url, presence: true, format: { with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.(sorry for unsplash)'}
 end
